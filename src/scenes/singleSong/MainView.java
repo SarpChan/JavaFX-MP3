566b02e1d59083b7fcdcd058c770f65e8ab183b5
mipetera <mike.peters11@web.de>
2018-12-14 19:06:26 +0100

    Das mit den eigenen Klassen damits schön aussieht musste ich wieder rückgängig machen.. dafür funktioniert jetzt die aktuelle zeit so halbwegs und der titel wird bei ein paar liedern angezeigt. gibt aber noch probleme mit der skalierung der Buttons etc.

--- src/scenes/singleSong/MainView.java
+++ src/scenes/singleSong/MainView.java
@@ -6,6 +6,11 @@ import Controller.PlaylistManager;
 import Exceptions.keinSongException;
 import controlElements.ControlButtons;
 import controlElements.VolumeAndTime;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -17,6 +22,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -33,6 +39,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.EventListener;
 
 public class MainView {
 
@@ -78,13 +87,12 @@ public class MainView {
             calculatePB(progress, pb1);
         });
 
-
-
         StackPane progressPane = new StackPane();
         progressPane.getChildren().addAll( line, pb1, progress);
         progressPane.setAlignment(Pos.CENTER_LEFT);
         progressPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         progressPane.setMinSize(Double.MIN_VALUE, Double.MIN_VALUE);
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -93,20 +101,175 @@ public class MainView {
         Text title = new Text(("Track" + " ").toUpperCase());
         title.getStyleClass().add("primarytext");
 
+
         Text interpret = new Text("Arctic Monkeys");
         interpret.getStyleClass().add("secondarytext");
 
-
         Pane songInfo = new HBox();
+        songInfo.setMaxWidth(20);
+        songInfo.setPrefWidth(20);
+        songInfo.setMinWidth(20);
         songInfo.getChildren().addAll(title, interpret);
         ((HBox) songInfo).setAlignment(Pos.CENTER_LEFT);
-        songInfo.setPadding(new Insets(0, 0, 0, 45));
+        songInfo.setPadding(new Insets(0, 100, 0, 45));
+
+
+        //VolumeAndTime
+
+        HBox volumeAndTime = new HBox();
+
+        ProgressBar pb2 = new ProgressBar(0.0);
+        pb2.minWidth(0);
+        pb2.setId("pb2");
+
+        Slider volume = new Slider();
+        volume.setId("volume");
+        volume.setMin(0);
+        volume.setMax(100);
+        volume.setValue(50);
+        volume.setOrientation(Orientation.HORIZONTAL);
+        volume.setPadding(new Insets(0, 0, 0, 10));
+        volume.boundsInLocalProperty().addListener((observable, oldvar, newvar) -> {
+            calculatePB(volume, pb2);
+        });
+        volume.valueProperty().addListener((observable, oldvar, newvar) -> {
+            calculatePB(volume, pb2);
+            player.volume((newvar.floatValue() / 100));
+        });
+
+        Button mute = new Button();
+        mute.getStyleClass().add("mute-button");
+        mute.setStyle("-fx-shape: \"" + getPathFromSVG("mute") + "\";");
+        mute.setAlignment(Pos.BASELINE_LEFT);
+        //mute.setOnAction(muteButtonOnAction(volume));
+        mute.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            if(volume.getValue()!= volume.getMin())
+                volume.setValue(volume.getMin());
+            else
+                volume.setValue(volume.getMax()/2);
+        });
+
+        Line line2 = new Line();
+        line2.setStartX(0);
+        line2.setEndX(100);
+        line2.getStyleClass().add("progressLine");
+
+        StackPane volumePane = new StackPane();
+        volumePane.getChildren().addAll(volume,pb2,line2);
+        volumePane.setAlignment(Pos.CENTER_LEFT);
+
+
+
+
+
+        DateFormat zeitanzeige = new SimpleDateFormat("mm:ss");
+        Text time = new Text();
+        time.getStyleClass().add("secondarytext");
+
+        Text songLength = new Text();
+        songLength.setText(zeitanzeige.format(player.getSongLength()));
+        long endTime = player.getSongLength();
+
+        final Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.millis( 500 ),
+                        event -> {
+
+
+                            if ( player.getAktZeit() == 0 ) {
+
+                                time.setText( zeitanzeige.format( 0 ) );
+                            } else if (player.getAktZeit() == endTime){
+                                try {
+                                    player.next();
+                                    title.setText(player.getTrack());
+                                    interpret.setText(player.getSongArtist());
+                                    songLength.setText(zeitanzeige.format(player.getSongLength()));
+                                } catch (keinSongException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                time.setText( zeitanzeige.format( player.getAktZeit() ) );
+                            }
+                        }
+                )
+        );
+        timeline.setCycleCount( Animation.INDEFINITE );
+        timeline.play();
+
+
+
+        volumeAndTime.getChildren().addAll(mute, volumePane, time);
+        volumeAndTime.setAlignment(Pos.CENTER);
+        volumeAndTime.setPadding(new Insets(0, 45, 0, 0));
 
         //ControlButtons
-        ControlButtons controlButtons = new ControlButtons(player);
-        //Volume&Time
-        VolumeAndTime volumeAndTime = new VolumeAndTime(player);
 
+        HBox controlButtons = new HBox(20);
+        Button play = new Button();
+        play.getStyleClass().add("play-button");
+        play.setStyle("-fx-shape: \"" + getPathFromSVG("play") + "\";");
+        play.setPadding(new Insets(0, 100, 0, 100));
+        play.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+
+            try {
+                if (player.isInitialized()) {
+                    if (!player.isPlaying()) {
+                        player.play();
+                        play.setStyle("-fx-shape: \"" + getPathFromSVG("pause") + "\";");
+
+                        title.setText(player.getTrack());
+                        interpret.setText(player.getSongArtist());
+                        time.setText("00/" + zeitanzeige.format(player.getSongLength()));
+                    } else {
+                        player.pause();
+                        play.setStyle("-fx-shape: \"" + getPathFromSVG("play") + "\";");
+                    }
+                } else {
+                    player.play();
+                    play.setStyle("-fx-shape: \"" + getPathFromSVG("pause") + "\";");
+                    title.setText(player.getTrack());
+                    interpret.setText(player.getSongArtist());
+                    time.setText(zeitanzeige.format(player.getSongLength()));
+                }
+
+            } catch (keinSongException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        Button previous = new Button();
+        previous.getStyleClass().add("icon-button");
+        previous.setStyle("-fx-shape: \"" + getPathFromSVG("previous") + "\";");
+        previous.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            try{
+                player.previous();
+                title.setText(player.getTrack());
+                interpret.setText(player.getSongArtist());
+                time.setText(zeitanzeige.format(player.getSongLength()));
+            } catch( keinSongException e){
+
+            }
+        });
+
+        Button next = new Button();
+        next.getStyleClass().add("icon-button");
+        next.setStyle("-fx-shape: \"" + getPathFromSVG("next") + "\";");
+        next.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            try {
+                player.next();
+                title.setText(player.getTrack());
+                interpret.setText(player.getSongArtist());
+                time.setText(zeitanzeige.format(player.getSongLength()));
+            }  catch (keinSongException e) {
+                e.printStackTrace();
+            }
+        });
+
+        controlButtons.getChildren().addAll(previous, play, next);
+        controlButtons.setAlignment(Pos.CENTER);
+        GridPane.setHalignment(controlButtons, HPos.CENTER);
 
         //HINTERGRUND
         Rectangle progressBackground = new Rectangle();
@@ -124,14 +287,13 @@ public class MainView {
 
         //REGIONS
         Pane region = new HBox();
-        region.setPrefWidth(50);
-        region.setMinWidth(50);
+        region.setPrefWidth(0);
+        region.setMinWidth(0);
         HBox.setHgrow(region, Priority.ALWAYS);
 
-
         Pane region2 = new HBox();
-        region2.setPrefWidth(50);
-        region2.setMinWidth(50);
+        region2.setPrefWidth(0);
+        region2.setMinWidth(0);
         HBox.setHgrow(region2, Priority.ALWAYS);
 
 
